8471e3ebf2842adc8d31fc4d1e80b568b10b446e
itsmedanz <58055809+AchmadDani@users.noreply.github.com>
2025-12-22 16:19:12 +0700

    Create greeting.sh

---
+++ greeting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## gunakan perintah USER untuk memunculkan user saat ini
+
+## gunakan perintah date untuk memunculkan tanggal dan waktu sekarang
+
+## gunakan perintah who untuk memunculkan user-user yang sedang login
